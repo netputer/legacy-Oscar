fa67a278eae4ca056835a2148c2efb3749fd3fcf
leplay <leplay@wandoujia.com>
2013-11-23 17:20:16 +0800

    add pos parameter to app's downloadUrl

--- app/javascripts/main/DownloadHelper.js
+++ app/javascripts/main/DownloadHelper.js
@@ -57,7 +57,7 @@
                 if (title === provider.title) {
                     console.log(provider)
                     var icon = provider.iconUrl;
-                    var url = provider.appDownloadUrl + '#name=' + title + '&icon=' + icon + '&content-type=application';
+                    var url = provider.appDownloadUrl + '?pos=oscar-promotion#name=' + title + '&icon=' + icon + '&content-type=application';
                     $('<a>').attr({'href' : url, 'download' : title})[0].click();
                     sessionStorage.setItem(title, 'installed');
                     return true;
