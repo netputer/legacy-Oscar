7e46862972db230f7c6ef76c8eee157a258f4172
leplay <leplay@wandoujia.com>
2013-12-23 13:21:11 +0800

    Revert " fix: 修复搜索页 url 变化时搜索框的关键词不变的问题"
    This reverts commit 668c733392a87de0acee9e439a5fd54de67e7377.

--- app/javascripts/components/searchbox/SearchBoxView.jsx
+++ app/javascripts/components/searchbox/SearchBoxView.jsx
@@ -120,8 +120,6 @@
                 _.each(this.state.resultModels, function (model) {
                     model.off('change:selected', this.modelChangeHander, this);
                 }, this);
-
-                this.refs['searchBoxInput'].getDOMNode().value = this.props.keyword;
             },
             componentDidUpdate : function () {
                 _.each(this.state.resultModels, function (model) {
