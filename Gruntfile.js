3c4f1bb3b6a14c82f0a503b8b50ad9c434f62c63
leplay <leqcany@gmail.com>
2014-05-21 16:00:45 +0800

    use grunt serve instead grunt server

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('server', [
+    grunt.registerTask('serve', [
         'shell:buildAdonis',
         'clean:server',
         'compass:server',
@@ -292,6 +292,11 @@ module.exports = function (grunt) {
         'open',
         'watch'
     ]);
+
+    grunt.registerTask('server', function () {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve']);
+    });
 
     grunt.registerTask('build', [
         'clean:dist',
