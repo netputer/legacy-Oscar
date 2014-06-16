44c74ab79e75e8d7473aa02674b3ae9b3a84e2cd
leplay <leqcany@gmail.com>
2014-06-16 18:20:01 +0800

    modify legal link

--- app/javascripts/components/DeclarationView.jsx
+++ app/javascripts/components/DeclarationView.jsx
@@ -28,7 +28,7 @@
                                 <h2 className="w-text-secondary">声明</h2>
                                 <p className="w-text-thirdly">你将开始使用豌豆荚的视频服务。</p>
                                 <p className="w-text-thirdly">继续使用表明你同意豌豆荚关于视频搜索、播放和缓存功能的声明。</p>
-                                <p className="w-text-info">点击查看<a href="http://www.wandoujia.com/mobile/video_legal.html" target="_default">声明全文</a></p>
+                                <p className="w-text-info">点击查看<a href="http://www.wandoujia.com/video_legal.html" target="_default">声明全文</a></p>
                                 <button className="w-btn w-btn-primary" onClick={this.hidePopup}>同意并继续</button>
                             </div>
                         </div>
