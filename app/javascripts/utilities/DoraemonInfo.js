b354204f323f603215e1205d7c977e971f0950d8
leplay <leqcany@gmail.com>
2014-07-08 20:18:27 +0800

    Release v2.9.3

--- app/javascripts/utilities/DoraemonInfo.js
+++ app/javascripts/utilities/DoraemonInfo.js
@@ -1,7 +1,7 @@
 (function (window) {
     define([], function () {
         var Doraemon = {
-            "version" : "2.9.2"
+            "version" : "2.9.3"
         };
 
         return Doraemon;
