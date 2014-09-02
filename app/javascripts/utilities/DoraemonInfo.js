c81f3920a5a648137ad94a8b51ead5608ee4d63a
leplay <leplay@wandoujia.com>
2014-09-02 18:47:05 +0800

    Release v3.0.11

--- app/javascripts/utilities/DoraemonInfo.js
+++ app/javascripts/utilities/DoraemonInfo.js
@@ -1,7 +1,7 @@
 (function (window) {
     define([], function () {
         var Doraemon = {
-            "version" : "3.0.10"
+            "version" : "3.0.11"
         };
 
         return Doraemon;
